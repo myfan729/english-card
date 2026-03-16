87079ea438f87b810e4547786b228ba6483003c7
myfan729 <52336820+myfan729@users.noreply.github.com>
2026-03-17 00:00:29 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-core");
+const chromium = require("@sparticuz/chromium");
 
 const app = express();
 app.use(express.json({ limit: "5mb" }));
@@ -13,12 +14,9 @@ app.post("/screenshot", async (req, res) => {
   let browser;
   try {
     browser = await puppeteer.launch({
-      args: [
-        "--no-sandbox",
-        "--disable-setuid-sandbox",
-        "--disable-dev-shm-usage",
-      ],
-      headless: "new",
+      args: chromium.args,
+      executablePath: await chromium.executablePath(),
+      headless: chromium.headless,
     });
 
     const page = await browser.newPage();
